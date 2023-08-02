e9d580519cedf8b914af2c45bccbe947491929a0
Mohit <adminmohit@0ypfn.onmicrosoft.com>
2023-08-02 16:31:43 +0530

    Sum of Two integers

---
+++ Integer_Sum/Controllers/AddController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integer_Sum.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class AddController : ControllerBase
+    {
+        public int Post(IntegerModel model)
+        {
+            if (model.FirstInteger == 0 || model.SecondInteger == 0)
+                throw new ArgumentNullException("FirstInteger or SecondInteger is required");
+
+            return model.FirstInteger + model.SecondInteger;
+        }
+    }
+}
---
+++ Integer_Sum/IntegerModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Integer_Sum
+{
+    public class IntegerModel
+    {
+        public int FirstInteger { get; set; }
+        public int SecondInteger { get; set; }
+    }
+}
